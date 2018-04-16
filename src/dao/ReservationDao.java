a3486f54cfb2ad6adee898bef703a41bb90cb94a
Stan <stanislavkl1996@gmail.com>
2018-04-17 01:42:11 +0300

    added a method in ReservationDao

--- src/dao/ReservationDao.java
+++ src/dao/ReservationDao.java
@@ -1,6 +1,8 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -27,25 +29,47 @@ public class ReservationDao implements IReservationDao {
 	}
 
 	@Override
-	public void addReservation(User u, Reservation r, ArrayList<Seat> seats) throws Exception {
-		// TODO Auto-generated method stub
+	public void addReservation(User u, Reservation r, ArrayList<Seat> seats) throws SQLException {
+		// TODO Auto-generated method stub	
+	}
+
+	@SuppressWarnings("resource")
+	@Override
+	public void deleteReservation(Reservation r) throws SQLException {
+		//Transaction remove from reservations table
+		// then remove all from reservations_seats where id = reservation id
 		
+		PreparedStatement s = null;
+		try {
+			connection.setAutoCommit(false);
+			s = connection.prepareStatement("DELETE FROM reservations WHERE id = ?");
+			s.setInt(1, r.getId());
+			s.executeUpdate();
+			
+			s =  connection.prepareStatement("DELETE FROM reservation_seats WHERE ticket_reservations_id = ?");
+			s.setInt(1, r.getId());
+			s.executeUpdate();
+			connection.commit();
+		}
+		catch (SQLException e){
+			connection.rollback();
+			throw e;
+		}
+		finally {
+			s.close();
+			connection.setAutoCommit(true);
+		}
+
 	}
 
 	@Override
-	public void deleteReservation(Reservation r) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Collection<Reservation> getAllReservations() throws Exception {
+	public Collection<Reservation> getAllReservations() throws SQLException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Collection<Reservation> getAllReservationsForABroadcast(Broadcast b) throws Exception {
+	public Collection<Reservation> getAllReservationsForABroadcast(Broadcast b) throws SQLException {
 		// TODO Auto-generated method stub
 		return null;
 	}
